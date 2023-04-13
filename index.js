c1ac653ae1a784e9e617984ef9935b3d6615d5c2
jammula alekh <jammula.prasad@liferay.com>
2023-04-13 12:43:23 +0530

    latest commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const menuButton = document.getElementById("menu-button")
 const menu = document.getElementById('menu')
-const ipad = window.matchMedia('screen and (max-width: 767px)')
 
 function hideShowMenu() {
     console.log(menu)
@@ -16,16 +15,6 @@ function hideShowMenu() {
     }
 }
 
-function validation(event) {
-    if (event.matches)
-        menuButton.addEventListener('click', hideShowMenu)
-    else
-        menuButton.removeEventListener('click', hideShowMenu)
-}
 
-if (ipad.matches)
-    menuButton.addEventListener('click', hideShowMenu)
-
-ipad.addEventListener('change', validation)
 
 
